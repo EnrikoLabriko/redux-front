c560aa40fd56bd3248d29eca11a0a270814c3909
Alexander Telyatnikov <labriko@yandex.ru>
2016-11-16 17:37:06 +0300

    Added react-hot-loader@v3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import 'babel-polyfill'
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import App from './containers/App'
 
-
-render(
-<App />,
+ReactDOM.render(
+  <AppContainer>
+    <App/>
+  </AppContainer>,
   document.getElementById('root')
 );
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    const NextApp = require('./containers/App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp/>
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
+    'react-hot-loader/patch',
     'webpack-hot-middleware/client',
     './src/index'
   ],
